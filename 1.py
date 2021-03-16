3946755a3bc40199b788edad400a014b2cd38035
CrystalBRana <crystalkoinch@gmail.com>
2021-03-16 09:56:47 +0545

    Initial commit

---
+++ 1.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound("E:\Music\Beth & Joe - I'd Rather Go Blind - Lmmive in Amsterdam.mp3")
---
+++ for_loop.py
@@ -0,0 +1,20 @@
+# Write a program to find the sum of each element of list?
+sum = 0
+li = [1, 2, 3, 4, 5, 6, 7, 8]
+for x in range(0 ,9):
+    sum = sum + x
+    print(sum)
+
+'''
+total = 0
+
+# creating a list
+list1 = [11, 5, 17, 18, 23]
+
+# Iterate each element in list
+# and add them in variale total
+for ele in range(0, len(list1)):
+    total = total + list1[ele]
+
+# printing total value
+print("Sum of all elements in given list: ", total)'''
